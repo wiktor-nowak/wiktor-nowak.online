52af3cd977a2cf76c6e5f7eea14912f692a50f3a
wiktor-nowak <nowak.wiktor95@gmail.com>
2024-11-22 15:57:39 +0100

    Initial

---
+++ src/components/acomponent.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./acomponent.module.scss";
+
+function AComponent() {
+  return <div className={styles.sth}>Somethibng</div>;
+}
+
+export default AComponent;
